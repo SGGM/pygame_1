cd5f0db1d72fafbfe90162d70826a5bb4f4fa6fb
SGGM <gleb_somov@mail.ru>
2021-07-27 01:03:39 +0300

    added quit in main, game now draws a circle

--- game.py
+++ game.py
@@ -1,13 +1,33 @@
 import pygame
 
 
+
+# Makes game window and caption
 WIDTH, HEIGHT = 500, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("The Game!")
+
+
+# Place for constants
+WHITE = (255, 255, 255)
+
+
+pygame.draw.circle(WIN, WHITE, (250, 250), 50)
+
+pygame.display.update()
 
 
 
 def main():
-    pass
+    
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+    pygame.quit()
+
 
 if __name__ == "__main__":
     main()
